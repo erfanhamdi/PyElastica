c5c520c094fc7fa12de896173f8f030110191621
erfanhamD <erfan.hamdi@gmail.com>
2023-04-29 15:43:30 +0330

    added plotting and other functions

--- examples/CatenaryCurve/catenary_curve.py
+++ examples/CatenaryCurve/catenary_curve.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Import modules
 from elastica.modules import BaseSystemCollection, Constraints, Forcing, Damping
@@ -10,22 +11,26 @@ from elastica.rod.cosserat_rod import CosseratRod
 from elastica.dissipation import AnalyticalLinearDamper
 
 # Import Boundary Condition Classes
-from elastica.boundary_conditions import OneEndFixedRod, FreeRod, FixedConstraint
-from elastica.external_forces import EndpointForces, GravityForces
+from elastica.boundary_conditions import FixedConstraint
+from elastica.external_forces import GravityForces
 
 # Import Timestepping Functions
 from elastica.timestepper.symplectic_steppers import PositionVerlet
 from elastica.timestepper import integrate
 
-class TimoshenkoBeamSimulator(BaseSystemCollection, Constraints, Forcing, Damping):
+class CatenaryCurveSimulator(BaseSystemCollection, Constraints, Forcing, Damping):
     pass
 
 
-timoshenko_sim = TimoshenkoBeamSimulator()
+catenary_sim = CatenaryCurveSimulator()
+
+# Options
+PLOT_FIGURE = True
+SAVE_FIGURE = False
+SAVE_RESULTS = False
 
 # setting up test params
 n_elem = 100
-
 density = 1000
 nu = 2e-3
 E = 1e6
@@ -39,6 +44,7 @@ normal = np.array([0.0, 1.0, 0.0])
 base_length = 5.4
 base_radius = 0.05
 base_area = np.pi * base_radius ** 2
+
 shearable_rod= CosseratRod.straight_rod(
     n_elem,
     start,
@@ -59,46 +65,45 @@ plt.plot(
         label="n=" + str(shearable_rod.n_elems),
     )
 
-# timoshenko_sim.clear()
-timoshenko_sim.append(shearable_rod)
+catenary_sim.append(shearable_rod)
+
 dl = base_length / n_elem
 dt = 0.01 * dl
-timoshenko_sim.dampen(shearable_rod).using(
+
+catenary_sim.dampen(shearable_rod).using(
     AnalyticalLinearDamper,
     damping_constant=nu,
     time_step=dt,
 )
 
-timoshenko_sim.constrain(shearable_rod).using(
+catenary_sim.constrain(shearable_rod).using(
     FixedConstraint,
     constrained_position_idx=(0,-1)
 )
+
 gravitational_acc = -9.80665
-timoshenko_sim.add_forcing_to(shearable_rod).using(
+catenary_sim.add_forcing_to(shearable_rod).using(
     GravityForces, acc_gravity=np.array([0.0, gravitational_acc, 0.0])
 )
-print("Forces added to the rod")
-timoshenko_sim.finalize()
-print("System finalized")
+
+catenary_sim.finalize()
 
 final_time = 500.0
 total_steps = int(final_time / dt)
-print("Total steps to take", total_steps)
 
 timestepper = PositionVerlet()
 
-integrate(timestepper, timoshenko_sim, final_time, total_steps)
+integrate(timestepper, catenary_sim, final_time, total_steps)
+if PLOT_FIGURE:
 
-def plot_timoshenko(shearable_rod):
-    import matplotlib.pyplot as plt
-
-    plt.plot(
+    fig = plt.figure(figsize=(10, 8), frameon=True, dpi=150)
+    ax = fig.add_subplot(111)
+    ax.plot(
         shearable_rod.position_collection[2, :],
         shearable_rod.position_collection[1, :],
         "b-",
         label="n=" + str(shearable_rod.n_elems),
     )
+    if SAVE_FIGURE:
+        fig.savefig("catenary_curve.pdf")
     plt.show()
-
-
-plot_timoshenko(shearable_rod)
